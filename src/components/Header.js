57d3f68472c3fa994a8d21002d5f0bf8896f1681
xybai0103 <xueyin0103@gmail.com>
2023-06-12 17:15:25 -0500

    comlete navigations.js, header.js and add background image for header

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navigation from './Navigation';
+
+export default function Header() {
+    return (
+        <header>
+          <h1>
+            Xueyin Bai
+          </h1>
+          <Navigation />
+       </header>
+    );
+}
+
---
+++ src/components/Navigation.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+function Navigation({ currentPage, handlePageChange }) {
+  return (
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <a
+          href="#about"
+          onClick={() => handlePageChange('About')}
+          className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+        >
+          About me
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#project"
+          onClick={() => handlePageChange('Project')}
+          className={currentPage === 'Project' ? 'nav-link active' : 'nav-link'}
+        >
+          Portfolio
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#contact"
+          onClick={() => handlePageChange('Contact')}
+          className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+        >
+          Contact
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#resume"
+          onClick={() => handlePageChange('Resume')}
+          className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+        >
+          Resume
+        </a>
+      </li>
+    </ul>
+  );
+}
+
+export default Navigation;
