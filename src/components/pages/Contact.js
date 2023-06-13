7c51af9cc59f871fa824713ac5b0e37c674dbdd7
xybai0103 <xueyin0103@gmail.com>
2023-06-13 14:56:03 -0500

    complete Contact.js

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,51 +1,101 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { validateEmail } from '../utils/helpers';
 
-export default function Contact(props) {
+export default function Contact() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleInputChange = (e) => {
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    if (inputType === 'Name') {
+      setName(inputValue);
+    } else if (inputType === 'Email') {
+      setEmail(inputValue);
+    } else if (inputType === 'Message') {
+      setMessage(inputValue);
+    }
+  };
+
+  const validateField = (e) => {
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    if (inputValue === '') {
+      setErrorMessage(`${inputType} is required`);
+      //prevent the form to be submitted
+      return false;
+    }
+
+    if (!validateEmail(email)) {
+      setErrorMessage(`Your email is invalid`);
+      return false;  
+    }
+    //allow the form to be submitted
+    return true;
+  }
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    setName('');
+    setEmail('');
+    setMessage('');
+  }
+
   return (
     <div>
       <h1>Contact</h1>
-      <form>
-        <div className='form-group'>
-            <label>Name:</label>
-            <input
-              onChange={props.handleInputChange}
-              value={props.value}
-              name="name"
-              type="text"
-              className="form-control"
-              id="name"
-            />
-            <label>Email address:</label>
-            <input
-              onChange={props.handleInputChange}
-              value={props.value}
-              name="email"
-              type="text"
-              className="form-control"
-              id="email"
-            />
-            <label>Message:</label>
-            <input
-              onChange={props.handleInputChange}
-              value={props.value}
-              name="message"
-              type="text"
-              className="form-control"
-              id="message"
-            />
-            <br />
-            <button
-              onClick={props.handleFormSubmit}
-              className='btn btn-primary'
-              type='submit'
-            >
-                Submit
-            </button>
-        </div>
+      <form className='form-group'>
+        <label>Name:</label>
+        <input
+            onChange={handleInputChange}
+            value={name}
+            name="Name"
+            type="text"
+            className="form-control"
+            id="name"
+            required
+            onBlur={validateField}
+        />
+        <label>Email address:</label>
+        <input
+            onChange={handleInputChange}
+            value={email}
+            name="Email"
+            type="email"
+            className="form-control"
+            id="email"
+            required
+            onBlur={validateField}
+        />
+        <label>Message:</label>
+        <input
+            onChange={handleInputChange}
+            value={message}
+            name="Message"
+            type="text"
+            className="form-control"
+            id="message"
+            required
+            onBlur={validateField}
+        />
+        {errorMessage &&  (
+            <p className='error-text'>{errorMessage}</p>
+        )}
+        <button
+            onClick={handleFormSubmit}
+            className='btn btn-primary'
+            type='submit'
+        >
+            Submit
+        </button>
       </form>
     </div>
   );
-}
-
-//Your email is invalid
-//Message is required
+};
