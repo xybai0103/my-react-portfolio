735a88bf403c8405efa9448a05caf464212bc551
Xueyin Bai <xueyin0103@gmail.com>
2023-08-21 22:50:34 -0500

    add and install webpack and other bundle dependencies

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import myResume from '../../assets/Xueyin Bai_Job Title_Resume.pdf';
+import myResume from '../../assets/Xueyin Bai_Resume.pdf';
 
 export default function Resume() {
   return (
